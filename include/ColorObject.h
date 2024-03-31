125f48b34266eba9d00d8cb73ee208e0dd250f4a
unknown <marbtc0505@gmail.com>
2024-03-31 12:11:09 +0200

    Repair file ColorObject.h and ColorObject.cpp

--- include/ColorObject.h
+++ include/ColorObject.h
@@ -7,7 +7,8 @@
 class ColorObject : public GameObject
 {
 public:
-	ColorObject(float r, float g, float b) : _r(r), _g(g), _b(b) {}
+	ColorObject(float r, float g, float b);
+	ColorObject();
 	virtual ~ColorObject() = default;
 	void SetColor(float r, float g, float b);
 	virtual void Draw() const = 0;
@@ -15,6 +16,31 @@ private:
 	float _r;
 	float _g;
 	float _b;
-};
 
+
+    GLuint _shaderProgram;
+    GLuint _vertexShader;
+    GLuint _fragmentShader;
+
+    std::string _vertexShaderSource = R"(
+        #version 330 core
+        layout(location = 0) in vec3 aPos;
+        void main()
+        {
+            gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+        }
+    )";
+
+    std::string _fragmentShaderSource = R"(
+        #version 330 core
+        out vec4 FragColor;
+        uniform vec3 color;
+        void main()
+        {
+            FragColor = vec4(color, 1.0);
+        }
+    )";
+
+    void CompileShaders();
+};
 #endif // INC_2DSPARK_COLOROBJECT_H
