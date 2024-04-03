c5ca0b61b6aa8d79b61766fd01d82ba6849d03fe
unknown <marbtc0505@gmail.com>
2024-04-03 18:50:26 +0200

    Dodanie klasy RecatngleObject

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,15 @@
 #include "../include/Engine.h"
 #include "../include/LineObject.h"
 #include "../include/CircleObject.h"
+#include "../include/RectangleObject.h"
 
 int main(void)
 {
     Engine engine(800, 600, "Test");
     LineObject line( Point(0.0f, 0.0f), Point(0.5f, 0.5f), 1.0f, 0.0f, 0.0f);
     CircleObject circle( Point(0.5f, 0.5f), Point(0.0f, 0.0f), 0.5f, 0.0f, 1.0f, 0.0f);
+    RectangleObject rectangle(Point(0.0f, -0.0f), Point(0.5f, 0.5f), 0.0f, 0.0f, 1.0f);
+    engine.GetRenderer()->AddObject(&rectangle);
     engine.GetRenderer()->AddObject(&circle);
     engine.GetRenderer()->AddObject(&line);
     engine.Run();
